9aa5d557f9d9fdf5462fe80d9869d4d3fb00c0ac
Shrey Chandpa <shreykc1@gmail.com>
2024-08-25 01:24:09 +0530

    First Commit (Auth Done/ Room Created)

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,12 @@
+import CollaborativeRoom from '@/components/ui/CollaborativeRoom'
+import React from 'react'
+
+const Document = () => {
+  return (
+    <main className='flex w-full flex-col items-center'>
+        <CollaborativeRoom/>
+    </main>
+  )
+}
+
+export default Document
